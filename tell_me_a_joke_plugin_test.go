0ce6ea60753c31f415090a4261b33a024425691f
Johannes Tuchscherer <jtuchscherer@gmail.com>
2017-10-09 20:28:45 +0200

    go fmt

--- tell_me_a_joke_plugin_test.go
+++ tell_me_a_joke_plugin_test.go
@@ -35,79 +35,76 @@ var _ = Describe("TellMeAJokePlugin", func() {
 		})
 
 		Context("when there is internet connection to the joke api", func() {
-			
-				Context("when the response is proper JSON", func() {
-					BeforeEach(func() {
 
-						server.AppendHandlers(ghttp.CombineHandlers(
-							ghttp.VerifyRequest("GET", "/"),
-							ghttp.VerifyHeaderKV("Accept", "application/json"),
-							ghttp.VerifyHeaderKV("User-Agent", "https://github.com/jtuchscherer/tell-me-joke-plugin"),
-							ghttp.RespondWith(200, `
+			Context("when the response is proper JSON", func() {
+				BeforeEach(func() {
+
+					server.AppendHandlers(ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/"),
+						ghttp.VerifyHeaderKV("Accept", "application/json"),
+						ghttp.VerifyHeaderKV("User-Agent", "https://github.com/jtuchscherer/tell-me-joke-plugin"),
+						ghttp.RespondWith(200, `
         				{
                   "id": "R7UfaahVfFd",
                   "joke": "My dog used to chase people on a bike a lot. It got so bad I had to take his bike away.",
                   "status": 200
                 }
 				      `),
-						))
-					})
-					It("displays the joke's text", func(done Done) {
-						defer close(done)
-						go invokeCmd(outputChan, tellMeAJokeCmd, fakeCliConnection)
-
-						var output []string
-						Eventually(outputChan, 2).Should(Receive(&output))
-						outputString := strings.Join(output, "")
-						Expect(outputString).To(Equal("My dog used to chase people on a bike a lot. It got so bad I had to take his bike away."))
-					})
+					))
 				})
+				It("displays the joke's text", func(done Done) {
+					defer close(done)
+					go invokeCmd(outputChan, tellMeAJokeCmd, fakeCliConnection)
 
-				Context("when the response is not in the expected JSON", func() {
-					BeforeEach(func() {
-						server.AppendHandlers(ghttp.CombineHandlers(
-							ghttp.VerifyRequest("GET", "/"),
-							ghttp.VerifyHeaderKV("Accept", "application/json"),
-							ghttp.VerifyHeaderKV("User-Agent", "https://github.com/jtuchscherer/tell-me-joke-plugin"),
-							ghttp.RespondWith(200, `something else`),
-						))
-					})
-					It("shows a sad error message", func(done Done) {
-						defer close(done)
-						go invokeCmd(outputChan, tellMeAJokeCmd, fakeCliConnection)
-
-						var output []string
-						Eventually(outputChan, 2).Should(Receive(&output))
-						outputString := strings.Join(output, "")
-						Expect(outputString).To(ContainSubstring("FAILED"))
-					})
+					var output []string
+					Eventually(outputChan, 2).Should(Receive(&output))
+					outputString := strings.Join(output, "")
+					Expect(outputString).To(Equal("My dog used to chase people on a bike a lot. It got so bad I had to take his bike away."))
 				})
-					Context("when the response is empty", func() {
-					BeforeEach(func() {
-						server.AppendHandlers(ghttp.CombineHandlers(
-							ghttp.VerifyRequest("GET", "/"),
-							ghttp.VerifyHeaderKV("Accept", "application/json"),
-							ghttp.VerifyHeaderKV("User-Agent", "https://github.com/jtuchscherer/tell-me-joke-plugin"),
-							ghttp.RespondWith(200, ""),
-						))
-					})
-					It("shows a sad error message", func(done Done) {
-						defer close(done)
-						go invokeCmd(outputChan, tellMeAJokeCmd, fakeCliConnection)
+			})
 
-						var output []string
-						Eventually(outputChan, 2).Should(Receive(&output))
-						outputString := strings.Join(output, "")
-						Expect(outputString).To(ContainSubstring("FAILED"))
-					})
+			Context("when the response is not in the expected JSON", func() {
+				BeforeEach(func() {
+					server.AppendHandlers(ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/"),
+						ghttp.VerifyHeaderKV("Accept", "application/json"),
+						ghttp.VerifyHeaderKV("User-Agent", "https://github.com/jtuchscherer/tell-me-joke-plugin"),
+						ghttp.RespondWith(200, `something else`),
+					))
 				})
-				
-			
+				It("shows a sad error message", func(done Done) {
+					defer close(done)
+					go invokeCmd(outputChan, tellMeAJokeCmd, fakeCliConnection)
+
+					var output []string
+					Eventually(outputChan, 2).Should(Receive(&output))
+					outputString := strings.Join(output, "")
+					Expect(outputString).To(ContainSubstring("FAILED"))
+				})
+			})
+			Context("when the response is empty", func() {
+				BeforeEach(func() {
+					server.AppendHandlers(ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/"),
+						ghttp.VerifyHeaderKV("Accept", "application/json"),
+						ghttp.VerifyHeaderKV("User-Agent", "https://github.com/jtuchscherer/tell-me-joke-plugin"),
+						ghttp.RespondWith(200, ""),
+					))
+				})
+				It("shows a sad error message", func(done Done) {
+					defer close(done)
+					go invokeCmd(outputChan, tellMeAJokeCmd, fakeCliConnection)
+
+					var output []string
+					Eventually(outputChan, 2).Should(Receive(&output))
+					outputString := strings.Join(output, "")
+					Expect(outputString).To(ContainSubstring("FAILED"))
+				})
+			})
 
 		})
 		Context("when there is no connection to the joke api", func() {
 			BeforeEach(func() {
-
 				server.Close()
 			})
 			It("shows a good error message", func(done Done) {
